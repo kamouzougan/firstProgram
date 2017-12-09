ad960cecf2528e79ea6dc7a2b6ce54c93096182d
Kangni <kamouzougan@gmail.com>
2017-12-09 17:27:43 -0500

    grade

---
+++ grade.py
@@ -0,0 +1,11 @@
+grade  = int( input("Please enter the grade :  ") )
+
+if grade <=  65 :
+
+      print  (" your grade is F : see you next year " )
+elif grade <=75 :
+    print("your grade is C : you passed ")
+elif grade <= 89 :
+    print("your grde is B : you passed ")
+elif grade <= 99 :
+    print("your grade is A : you are excellent ")
