269c82506318b0a7822153d82ab7f00ef159ee14
Kangni <kamouzougan@gmail.com>
2017-12-09 12:15:38 -0500

    Divisor

---
+++ Divisor.py
@@ -0,0 +1,16 @@
+num =int( input("Please give me a number"))
+
+"""for element in x :
+
+  print(element)"""
+listRange = list(range(1,num + 1))
+
+divisorList = []
+for i in listRange :
+ if  num % i == 0 :
+  divisorList.append(i)
+  print(listRange)
+print(divisorList)
+
+
+
